8dcf38ac086b86fe8c896d15565c6ee9246ff467
melanmeg <naoya05280708@gmail.com>
2024-11-19 01:49:38 +0900

    fix

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+locals {
+  region              = "asia-northeast1"
+  zones               = ["asia-northeast1-a"]
+  project_id          = "test-project-373118"
+  github_repository   = "melanmeg/test2-workload-identity"
+
+  workload_identity_pool_id          = "my-github-pool6"
+  workload_identity_pool_provider_id = "my-github-pool-provider6"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.1.0"
+    }
+  }
+}
+
+provider "google" {
+  project        = local.project_id
+  region         = local.region
+  zone           = local.zones[0]
+}
+
+resource "google_iam_workload_identity_pool" "github_pool" {
+  project                   = local.project_id
+  workload_identity_pool_id = local.workload_identity_pool_id
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_pool_provider" {
+  project                            = local.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = local.workload_identity_pool_provider_id
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+  }
+  attribute_condition = "assertion.repository == '${local.github_repository}'"
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}

b67f7764bd7ba7d62a7c08ad72899ee2fba6ca43
melanmeg <naoya05280708@gmail.com>
2024-11-19 02:10:44 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,3 +48,27 @@ resource "google_artifact_registry_repository" "repository" {
   repository_id = "my-repository"
   format        = "DOCKER"
 }
+
+resource "google_service_account" "github_service_account" {
+  project      = local.project_id
+  account_id   = "my-github-sa"
+}
+
+resource "google_service_account_iam_member" "github_workload_identity_user" {
+  service_account_id = google_service_account.github_service_account.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.github_repository}"
+  depends_on         = [google_service_account.github_service_account]
+}
+
+resource "google_project_iam_member" "artifact_registry_reader" {
+  project = local.project_id
+  role    = "roles/artifactregistry.reader"
+  member  = google_service_account.github_service_account.member
+}
+
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = local.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = google_service_account.github_service_account.member
+}

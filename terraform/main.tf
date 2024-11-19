fb450bfe3e44f880d36a9fb4b4e5d52cf1834388
melanmeg <naoya05280708@gmail.com>
2024-11-19 12:34:29 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,42 +43,16 @@ resource "google_iam_workload_identity_pool_provider" "github_pool_provider" {
   }
 }
 
-resource "google_artifact_registry_repository" "my_repo" {
+resource "google_artifact_registry_repository" "repository" {
   location      = local.region
   repository_id = "my-repository"
   format        = "DOCKER"
 }
 
-resource "google_artifact_registry_repository_iam_member" "my_repo_editor" {
+resource "google_artifact_registry_repository_iam_member" "artifact_registry_writer" {
   project    = local.project_id
-  repository = google_artifact_registry_repository.my_repo.name
-  location   = google_artifact_registry_repository.my_repo.location
+  repository = google_artifact_registry_repository.repository.name
+  location   = google_artifact_registry_repository.repository.location
   role       = "roles/artifactregistry.writer"
   member     = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.github_repository}"
 }
-
-# ERROR: denied: Permission "artifactregistry.repositories.uploadArtifacts" denied on resource "projects/test-project-373118/locations/asia-northeast1/repositories/my-repository" (or it may not exist)
-
-# resource "google_service_account" "github_service_account" {
-#   project      = local.project_id
-#   account_id   = "my-github-sa"
-# }
-
-# resource "google_service_account_iam_member" "github_workload_identity_user" {
-#   service_account_id = google_service_account.github_service_account.id
-#   role               = "roles/iam.workloadIdentityUser"
-#   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.github_repository}"
-#   depends_on         = [google_service_account.github_service_account]
-# }
-
-# resource "google_project_iam_member" "artifact_registry_reader" {
-#   project = local.project_id
-#   role    = "roles/artifactregistry.reader"
-#   member  = google_service_account.github_service_account.member
-# }
-
-# resource "google_project_iam_member" "artifact_registry_writer" {
-#   project = local.project_id
-#   role    = "roles/artifactregistry.writer"
-#   member  = google_service_account.github_service_account.member
-# }

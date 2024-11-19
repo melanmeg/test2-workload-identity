b5cbf28e7e7bc51db33625bc86766ae095d04ae2
melanmeg <naoya05280708@gmail.com>
2024-11-19 11:27:16 +0900

    fix

--- shell.sh
+++ shell.sh
@@ -1,28 +1,51 @@
-#!/bin/bash
+#!/bin/bash -x
+
 PROJECT_ID=test-project-373118
 GITHUB_ORG=melanmeg
+WORKLOAD_IDENTITY_POOL_ID=projects/593997455442/locations/global/workloadIdentityPools/github
+REPO=melanmeg/test2-workload-identity
 
-gcloud iam workload-identity-pools create "github" \
-  --project="${PROJECT_ID}" \
-  --location="global" \
-  --display-name="GitHub Actions Pool"
+# gcloud iam workload-identity-pools create "github" \
+#   --project="${PROJECT_ID}" \
+#   --location="global" \
+#   --display-name="GitHub Actions Pool"
 
-gcloud iam workload-identity-pools describe "github" \
-  --project="${PROJECT_ID}" \
-  --location="global" \
-  --format="projects/593997455442/locations/global/workloadIdentityPools/github"
+# gcloud iam workload-identity-pools describe "github" \
+#   --project="${PROJECT_ID}" \
+#   --location="global" \
+#   --format="value(name)"
+# # projects/593997455442/locations/global/workloadIdentityPools/github
 
-gcloud iam workload-identity-pools providers create-oidc "my-repo" \
-  --project="${PROJECT_ID}" \
-  --location="global" \
-  --workload-identity-pool="github" \
-  --display-name="My GitHub repo Provider" \
-  --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner" \
-  --attribute-condition="assertion.repository_owner == '${GITHUB_ORG}'" \
-  --issuer-uri="https://token.actions.githubusercontent.com"
+# gcloud iam workload-identity-pools providers create-oidc "my-repo" \
+#   --project="${PROJECT_ID}" \
+#   --location="global" \
+#   --workload-identity-pool="github" \
+#   --display-name="My GitHub repo Provider" \
+#   --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner" \
+#   --attribute-condition="assertion.repository_owner == '${GITHUB_ORG}'" \
+#   --issuer-uri="https://token.actions.githubusercontent.com"
 
-gcloud iam workload-identity-pools providers describe "my-repo" \
-  --project="${PROJECT_ID}" \
-  --location="global" \
-  --workload-identity-pool="github" \
-  --format="projects/593997455442/locations/global/workloadIdentityPools/github"
+# gcloud iam workload-identity-pools providers describe "my-repo" \
+#   --project="${PROJECT_ID}" \
+#   --location="global" \
+#   --workload-identity-pool="github" \
+#   --format="value(name)"
+# # projects/593997455442/locations/global/workloadIdentityPools/github/providers/my-repo
+
+# gcloud secrets add-iam-policy-binding "my-secret" \
+#   --project="${PROJECT_ID}" \
+#   --role="roles/secretmanager.secretAccessor" \
+#   --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}"
+
+# gcloud artifacts repositories add-iam-policy-binding "my-repo" \
+#   --project="${PROJECT_ID}" \
+#   --role="roles/artifactregistry.admin" \
+#   --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}"
+
+# gcloud artifacts repositories add-iam-policy-binding my-repo \
+# --location=us-central1 --member=allUsers --role=roles/artifactregistry.reader [4]
+
+gcloud artifacts repositories add-iam-policy-binding "my-repo" \
+  --location="asia-northeast1" \
+  --role="roles/artifactregistry.writer" \
+  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${REPO}"
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ locals {
   project_id          = "test-project-373118"
   github_repository   = "melanmeg/test2-workload-identity"
 
-  workload_identity_pool_id          = "my-github-pool6"
-  workload_identity_pool_provider_id = "my-github-pool-provider6"
+  workload_identity_pool_id          = "my-github-pool7"
+  workload_identity_pool_provider_id = "my-github-pool-provider7"
 }
 
 terraform {
@@ -43,32 +43,39 @@ resource "google_iam_workload_identity_pool_provider" "github_pool_provider" {
   }
 }
 
-resource "google_artifact_registry_repository" "repository" {
+resource "google_artifact_registry_repository" "my_repo" {
   location      = local.region
   repository_id = "my-repository"
   format        = "DOCKER"
 }
 
-resource "google_service_account" "github_service_account" {
-  project      = local.project_id
-  account_id   = "my-github-sa"
+resource "google_artifact_registry_repository_iam_member" "my_repo_editor" {
+  repository = google_artifact_registry_repository.my_repo.name
+  location   = google_artifact_registry_repository.my_repo.location
+  role       = "roles/artifactregistry.writer"
+  member     = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.github_repository}"
 }
 
-resource "google_service_account_iam_member" "github_workload_identity_user" {
-  service_account_id = google_service_account.github_service_account.id
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.github_repository}"
-  depends_on         = [google_service_account.github_service_account]
-}
+# resource "google_service_account" "github_service_account" {
+#   project      = local.project_id
+#   account_id   = "my-github-sa"
+# }
 
-resource "google_project_iam_member" "artifact_registry_reader" {
-  project = local.project_id
-  role    = "roles/artifactregistry.reader"
-  member  = google_service_account.github_service_account.member
-}
+# resource "google_service_account_iam_member" "github_workload_identity_user" {
+#   service_account_id = google_service_account.github_service_account.id
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.github_repository}"
+#   depends_on         = [google_service_account.github_service_account]
+# }
 
-resource "google_project_iam_member" "artifact_registry_writer" {
-  project = local.project_id
-  role    = "roles/artifactregistry.writer"
-  member  = google_service_account.github_service_account.member
-}
+# resource "google_project_iam_member" "artifact_registry_reader" {
+#   project = local.project_id
+#   role    = "roles/artifactregistry.reader"
+#   member  = google_service_account.github_service_account.member
+# }
+
+# resource "google_project_iam_member" "artifact_registry_writer" {
+#   project = local.project_id
+#   role    = "roles/artifactregistry.writer"
+#   member  = google_service_account.github_service_account.member
+# }
